071c000e914c7e03c84503c1f742666695838777
Amir Jannatpour <jannatpour@hotmail.com>
2019-10-15 14:07:39 -0700

    first commit

---
+++ costarcast/src/main/java/cc/agileintelligence/costarcast/CostarcastApplication.java
@@ -0,0 +1,13 @@
+package cc.agileintelligence.costarcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostarcastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CostarcastApplication.class, args);
+    }
+
+}
